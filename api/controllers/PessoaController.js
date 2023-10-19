6447b823c6f3ac8b27ab0ba714341138787eccd0
Mikael Melo <mikaelsilva1940@hotmail.com>
2023-10-19 16:34:35 -0300

    add router put and delete

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,18 +1,68 @@
-const database = require('../models')
-
+const database = require("../models");
 
 class PessoaController {
-static async  pegaTodasAsPessoas (req,res) {
- try {
-  const todasAsPessoas = await database.Pessoas.findAll()
-  //tem que por o res e o return
-  return res.status(200).json({todasAsPessoas})
-   
- } catch( error) {
-  return res.status(500).json({message: `Aconteceu o seguinte erro ${error}`})
- }
+  //pega todas as pessoas
+  static async pegaTodasAsPessoas(req, res) {
+    try {
+      const todasAsPessoas = await database.Pessoas.findAll();
+      return res.status(200).json({ todasAsPessoas });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: `Aconteceu o seguinte erro ${error}` });
+    }
+  }
+
+  // procura uma pessoa database
+  static async pegaUmaPessoa(req, res) {
+    try {
+      const { id } = req.params; //parametro da requisição
+      const UmaPessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({ UmaPessoa });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  //cria pessoa
+  static async criaPessoa(req, res) {
+    try {
+      const novaPessoa = req.body;
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  //apaga pessoa
+  static async deletePessoa(req, res) {
+    try {
+      const { id } = req.params;
+      const deletePessoa = await database.Pessoas.destroy({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(deletePessoa);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  //atualizando pessoa
+  static async AtualizaPessoa(req, res) {
+    try {
+      const novaInfos = req.body;
+      const { id } = req.params;
+
+      await database.Pessoas.update(novaInfos, { where: { id: Number(id) } });
+      const pessoaAtualiza = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualiza);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
-}
-
-module.exports = PessoaController
+module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,6 +5,10 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.delete('/pessoas/:id', PessoaController.deletePessoa)
+router.put('/pessoas/:id', PessoaController.AtualizaPessoa)
 
 
 module.exports = router
